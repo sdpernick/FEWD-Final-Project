fe86b1bb2e253a1e0da44342f1889f64acb5ec2f
sdpernick <sarah.pernick@gmail.com>
2016-10-23 12:48:12 -0400

    HTML CSS changes

--- js/main.js
+++ js/main.js
@@ -5,12 +5,35 @@ $(".about").on("click",function(){
    $(".foodie1").slideToggle();
 });
 
-/*Filters*/
-$(".hood").hide();
 
-$(".li1").on("click", function(){
-   $(".hood").slideToggle();
+/*Filters*/
+$(".categories .hood").on("click", function(){
+   $(".sub-categories .hood").slideToggle();
 });
+
+$(".categories .cuisine").on("click", function(){
+   $(".sub-categories .cuisine").slideToggle();
+});
+
+$(".categories .price").on("click", function(){
+   $(".sub-categories .price").slideToggle();
+});
+
+$(".categories .good").on("click", function(){
+   $(".sub-categories .good").slideToggle();
+});
+
+$(".categories .atmo").on("click", function(){
+   $(".sub-categories .atmo").slideToggle();
+});
+
+/* do this instead of the above:
+$(".categories .hood").on("click", function(){
+   $(".sub-categories .hood").slideToggle();
+});
+
+
+Original Code
 
 $(".cuisine").hide();
 
@@ -35,3 +58,4 @@ $(".atmo").hide();
 $(".li5").on("click", function(){
    $(".atmo").slideToggle();
 });
+/*
