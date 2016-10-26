9bf7f537f606d835dc483b3d4b437fb2f0ea5a15
sdpernick <sarah.pernick@gmail.com>
2016-10-26 17:36:13 -0400

    Feedback

--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
-/*About*/
-$(".foodie1").hide();
-
-$(".about").on("click",function(){
-   $(".foodie1").slideToggle();
-});
-
-
 /*Filters*/
+
 $(".categories .hood").on("click", function(){
    $(".sub-categories .hood").slideToggle();
 });
@@ -27,35 +20,4 @@ $(".categories .atmo").on("click", function(){
    $(".sub-categories .atmo").slideToggle();
 });
 
-/* do this instead of the above:
-$(".categories .hood").on("click", function(){
-   $(".sub-categories .hood").slideToggle();
-});
 
-
-Original Code
-
-$(".cuisine").hide();
-
-$(".li2").on("click", function(){
-   $(".cuisine").slideToggle();
-});
-
-$(".price").hide();
-
-$(".li3").on("click", function(){
-   $(".price").slideToggle();
-});
-
-$(".good").hide();
-
-$(".li4").on("click", function(){
-   $(".good").slideToggle();
-});
-
-$(".atmo").hide();
-
-$(".li5").on("click", function(){
-   $(".atmo").slideToggle();
-});
-/*
